72bb52736e3f681b21266b14f2fe05af8e6bb759
admin <cedric.cam@hotmail.fr>
2021-11-29 17:05:42 +0100

    update ex04

--- Day00/ex04/program.js
+++ Day00/ex04/program.js
@@ -1,26 +1,29 @@
 function levDist(str1, str2){
-    let diffCount=0
-
-    if (str1.length < str2.length) {
-        strToModify = str2;
-        strToCopy = str1;
-    } else {
-        strToModify = str1;
-        strToModify = str2;
-    }
-
-    for (let i=0;i <= strToModify.length; i++) {
-        let character = strToModify.charAt(i);
-        for (let j=0; j <= strToCopy.length; j++) {
-            if (strToCopy.indexOf(character) === -1) {
-                diffCount++;
+    let diffCount1=0;
+    let diffCount2=0;
+    
+    for (let i=0;i <= str1.length; i++) {
+        let character = str1.charAt(i);
+        for (let j=0; j <= str2.length; j++) {
+            if (str2.indexOf(character) === -1) {
+                diffCount1++;
                 break;
             }
         }
     }
 
-    return diffCount;
+    for (let i=0;i <= str2.length; i++) {
+        let character = str2.charAt(i);
+        for (let j=0; j <= str1.length; j++) {
+            if (str1.indexOf(character) === -1) {
+                diffCount2++;
+                break;
+            }
+        }
+    }
+    return Math.max(diffCount1, diffCount2);
   };
+
  
 // Merci de ne pas effacer la ligne en dessous.
 exports.levDist =  levDist;
