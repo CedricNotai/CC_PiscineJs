08d446777f9451696a7c2951120edaa530b1a438
admin <cedric.cam@hotmail.fr>
2021-11-29 17:07:37 +0100

    update ex00

--- Day00/ex00/program.js
+++ Day00/ex00/program.js
@@ -12,7 +12,5 @@ function twoNumberSum(array, targetSum){
     return result;
 }
 
-console.log(twoNumberSum([3, 5, -4, 8, 11, 1, -1, 6], 15));
-
 // Merci de ne pas effacer la ligne en dessous.
-//exports.twoNumberSum =  twoNumberSum;
+exports.twoNumberSum =  twoNumberSum;
