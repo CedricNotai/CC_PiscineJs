f7f64aba64d2c46062ca4ad1fd628e66addf3297
admin <cedric.cam@hotmail.fr>
2021-11-29 15:20:28 +0100

    commit ex04

---
+++ Day00/ex04/program.js
@@ -0,0 +1,28 @@
+function levDist(str1, str2){
+    let diffCount1=0;
+    let diffCount2=0;
+
+    for (let i=0;i <= str1.length; i++) {
+        let character = str1.charAt(i);
+        for (let j=0; j <= str2.length; j++) {
+            if (str2.indexOf(character) === -1) {
+                diffCount1++;
+                break;
+            }
+        }
+    }
+
+    for (let i=0;i <= str2.length; i++) {
+        let character = str2.charAt(i);
+        for (let j=0; j <= str1.length; j++) {
+            if (str1.indexOf(character) === -1) {
+                diffCount2++;
+                break;
+            }
+        }
+    }
+    return Math.max(diffCount1, diffCount2);
+  };
+ 
+// Merci de ne pas effacer la ligne en dessous.
+exports.levDist =  levDist;
