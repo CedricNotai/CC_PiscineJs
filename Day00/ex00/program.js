974a9945385a2fff395fccd02173c9da95c5d8b5
admin <cedric.cam@hotmail.fr>
2021-11-29 13:53:28 +0100

    update

--- Day00/ex00/program.js
+++ Day00/ex00/program.js
@@ -2,16 +2,13 @@ function twoNumberSum(array, targetSum){
     let sum;
     let result = [];
     for (i=0; i < array.length; i++) {
-        for (j=0; j < array.length - i; j++ ) {
-            if (i != i+j) {
+        for (j=0; j < array.length -i; j++ ) {
                 sum=array[i]+array[i+j];
-                if (sum == targetSum) {
+                if (sum == targetSum && i!=i+j) {
                     result.push([array[i],array[i+j]]);
                 }
-            }
         }
     }
-    return result;
 }
     
 // Merci de ne pas effacer la ligne en dessous.
---
+++ Day00/ex03/program.js
@@ -0,0 +1,12 @@
+function firstNonRepeatingCharacter(string) {
+    for (let i=0; i<string.length; i++) {
+        let character = string.charAt(i);
+        if (string.indexOf(character) == i && string.indexOf(character, i +1) == -1) {
+            return i;
+        }
+    }
+    return -1;
+}
+    
+// Do not edit the line below.
+exports.firstNonRepeatingCharacter = firstNonRepeatingCharacter;
