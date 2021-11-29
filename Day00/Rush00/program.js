cc18dc20ebbf95db7c79390497cb35a72edf1147
admin <cedric.cam@hotmail.fr>
2021-11-29 15:50:49 +0100

    commit rush

---
+++ Day00/Rush00/program.js
@@ -0,0 +1,28 @@
+function sun(buildings, direction){
+    let results = [];
+
+    if (direction.toLowerCase() == "ouest") {
+        results.push(0);
+        for (let i=1; i < buildings.length-i; i++) {
+            if (buildings[i] > buildings[i-1]) {
+                results.push(i);
+            }
+        }
+    }
+
+    if (direction.toLowerCase() == "est") {
+        results.push(buildings.length-1);
+        for (let i=0; i < buildings.length-1; i++) {
+            if (buildings[i] > buildings[i-1]) {
+                results.push(i);
+            }
+        }
+    }
+
+    return results.sort(function(a,b){
+        return a - b;
+    });;
+}
+
+// Merci de ne pas effacer la ligne en dessous.
+exports.sun = sun ;
