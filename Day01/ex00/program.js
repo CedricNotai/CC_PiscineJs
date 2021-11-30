57c53702fa54424908a335a3ff89de5d5a37236d
Cédric CAM <cedriccam@MacBook-Pro-de-Cedric-2.local>
2021-11-30 09:52:10 +0100

    update day01ex00

--- Day01/ex00/program.js
+++ Day01/ex00/program.js
@@ -1,5 +1,3 @@
-let array = [ 1, 2, 3, 4, 5, 6];
-
 function tabTriCarre(array){
     for (let i=0; i < array.length; i++) {
         array[i] = Math.pow(array[i],2);
@@ -9,8 +7,6 @@ function tabTriCarre(array){
         return a - b;
     });
 }
-
-console.log(tabTriCarre(array));
     
 // Merci de ne pas effacer la ligne en dessous.
-//exports.tabTriCarre =  tabTriCarre;
+exports.tabTriCarre =  tabTriCarre;
